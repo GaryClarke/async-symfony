26e9a2ad525e675ff68a59134d1733ae37d36b7d
Gary Clarke <garyclarke1974@gmail.com>
2022-08-05 18:26:50 +0100

    Add code to SaveOrder files

--- src/MessageHandler/Command/SaveOrderHandler.php
+++ src/MessageHandler/Command/SaveOrderHandler.php
@@ -1,10 +1,19 @@
 <?php
 
-
 namespace App\MessageHandler\Command;
 
+use App\Message\Command\SaveOrder;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
-class SaveOrderHandler
+class SaveOrderHandler implements MessageHandlerInterface
 {
+    public function __invoke(SaveOrder $saveOrder)
+    {
+        // Save an order to the database
+        $orderId = 123;
 
+        echo 'Order being saved' . PHP_EOL;
+
+        // Dispatch an event message on an event bus
+    }
 }
